a37ad0d44ddf7870c504cc90a900f4245b00503c
Frop Core Labs <fropcore@proton.me>
2025-08-08 19:12:56 -0400

    Added Theme Files

---
+++ drudge-theme/footer.php
@@ -0,0 +1,14 @@
+<?php if (!defined('ABSPATH')) { exit; } ?>
+</main>
+<footer class="footer container">
+<div class="col-md-8">
+  <p>
+    &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Powered by 
+    <a href="https://github.com/SudoSeagull/NewsREPORT" target="_blank" rel="noopener">NewsREPORT</a> from
+        <a href="https://github.com/SudoSeagull" target="_blank" rel="noopener">SudoSeagull</a>
+  </p>
+</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
